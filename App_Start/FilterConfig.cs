c02d22f3e4745b69785c94c99e7aabb6faa51504
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2014-05-12 13:49:57 +0530

    code commit

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_WEBAPI_Pagination
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5_WEBAPI_Pagination.MvcApplication" Language="C#" %>
